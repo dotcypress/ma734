98028661129a200fd9b98dda0223bba122089ea6
Vitaly Domnikov <dotcypress@gmail.com>
2023-03-25 16:33:12 -0700

    0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -40,34 +40,148 @@ where
 
     /// Read current angle.
     pub fn read_angle(&mut self) -> Result<u16, Error<SPI>> {
-        let mut scratch = [0; 2];
-        self.tx(|spi| spi.transfer(&mut scratch))?;
-        Ok(u16::from_be_bytes(scratch))
+        let mut buf = [0x00; 2];
+        self.tx(|spi| spi.transfer(&mut buf))?;
+        Ok(u16::from_be_bytes(buf))
+    }
+
+    /// Get zero position angle
+    pub fn get_zero_angle(&mut self) -> Result<u16, Error<SPI>> {
+        let angle = u16::from_le_bytes([self.read_register(0x00)?, self.read_register(0x01)?]);
+        Ok(angle)
+    }
+
+    /// Set zero position angle
+    pub fn set_zero_angle(&mut self, angle: u16) -> Result<(), Error<SPI>> {
+        self.write_register(0x00, angle as _)?;
+        self.write_register(0x01, (angle >> 8) as _)?;
+        Ok(())
+    }
+
+    /// Get bias current trimming settings
+    pub fn get_bias_current_trimming(&mut self) -> Result<BiasCurrentTrimming, Error<SPI>> {
+        let val = self.read_register(0x02)?;
+        let axis_trims = self.read_register(0x03)?;
+        Ok(BiasCurrentTrimming {
+            val,
+            trim_x: axis_trims & 0b1 > 0,
+            trim_y: axis_trims & 0b10 > 0,
+        })
+    }
+
+    /// Set bias current trimming settings
+    pub fn set_bias_current_trimming(
+        &mut self,
+        bct: BiasCurrentTrimming,
+    ) -> Result<(), Error<SPI>> {
+        self.write_register(0x02, bct.val)?;
+        self.write_register(0x03, bct.trim_x as u8 | (bct.trim_y as u8) << 1)
+            .map(|_| ())
+    }
+
+    /// Get angle change interrupt config
+    pub fn get_angle_change_interrupt(&mut self) -> Result<AngleChangeInterrupt, Error<SPI>> {
+        let aci = self.read_register(0x07)?;
+        let threshold = self.read_register(0x08)?;
+        let reference = self.read_register(0x0a)?;
+        Ok(AngleChangeInterrupt {
+            threshold,
+            reference,
+            hysteresis: aci & 0b11111,
+            autoupdate: aci & 0b1000000 > 0,
+            mode: if aci & 0b10000000 > 0 {
+                IRQMode::LatchOff
+            } else {
+                IRQMode::Logic
+            },
+        })
+    }
+
+    /// Set angle change interrupt config
+    pub fn set_angle_change_interrupt(
+        &mut self,
+        aci: AngleChangeInterrupt,
+    ) -> Result<(), Error<SPI>> {
+        let mode_bits = aci.hysteresis & 0b11111;
+        let mode_bit = (aci.mode == IRQMode::LatchOff) as u8;
+        let autoupdate_bit = aci.autoupdate as u8;
+        self.write_register(0x07, mode_bits | autoupdate_bit << 6 | mode_bit << 7)?;
+        self.write_register(0x08, aci.threshold)?;
+        self.write_register(0x0a, aci.reference).map(|_| ())
+    }
+
+    /// Get magnetic field thresholds
+    pub fn get_magnetic_thresholds(&mut self) -> Result<MagneticFieldThresholds, Error<SPI>> {
+        let val = self.read_register(0x06)?;
+        Ok(MagneticFieldThresholds {
+            enable: val & 0b1 > 0,
+            low: (val >> 2) & 0b111,
+            high: (val >> 5) & 0b111,
+        })
+    }
+
+    /// Set magnetic field thresholds
+    pub fn set_magnetic_field_thresholds(
+        &mut self,
+        mft: MagneticFieldThresholds,
+    ) -> Result<(), Error<SPI>> {
+        let payload = mft.enable as u8 | (mft.low & 0b111) << 2 | (mft.high & 0b111) << 5;
+        self.write_register(0x06, payload).map(|_| ())
+    }
+
+    /// Set rotation direction
+    pub fn set_rotation_dir(&mut self, val: Dir) -> Result<(), Error<SPI>> {
+        self.write_register(0x09, val.into()).map(|_| ())
+    }
+
+    /// Get rotation direction
+    pub fn get_rotation_dir(&mut self) -> Result<Dir, Error<SPI>> {
+        self.read_register(0x09).map(From::from)
+    }
+
+    /// Set filter window
+    pub fn set_filter_window(&mut self, val: u8) -> Result<(), Error<SPI>> {
+        self.write_register(0x0e, val << 4).map(|_| ())
+    }
+
+    /// Get filter window
+    pub fn get_filter_window(&mut self) -> Result<u8, Error<SPI>> {
+        self.read_register(0x0e).map(|w| w >> 4)
     }
 
     /// Write register
     pub fn write_register(&mut self, reg: u8, val: u8) -> Result<u8, Error<SPI>> {
-        self.cmd(0x80 | (reg & 0x1f), val).map(|resp| resp[3])
+        self.cmd(0x80 | (reg & 0x1f), val)
     }
 
     /// Read register
     pub fn read_register(&mut self, reg: u8) -> Result<u8, Error<SPI>> {
-        self.cmd(0x40 | (reg & 0x1f), 0x0).map(|resp| resp[3])
+        self.cmd(0x40 | (reg & 0x1f), 0x00)
     }
 
     /// Store Register into the NVM
     pub fn store_register_into_nvm(&mut self, reg: u8) -> Result<(), Error<SPI>> {
-        self.cmd(0xe0 | (reg & 0x1f), 0x0).map(|_| ())
+        self.cmd(0xe0 | (reg & 0x1f), 0x00).map(|_| ())
     }
 
     /// Store All Registers into the NVM
     pub fn store_all_registers_into_nvm(&mut self) -> Result<(), Error<SPI>> {
-        self.cmd(0xc0, 0x0).map(|_| ())
+        self.cmd(0xc0, 0x00).map(|_| ())
     }
 
     /// Restore All Registers from the NVM
     pub fn restore_all_registers_from_nvm(&mut self) -> Result<(), Error<SPI>> {
-        self.cmd(0xa0, 0x0).map(|_| ())
+        self.cmd(0xa0, 0x00).map(|_| ())
+    }
+
+    /// Get magnetic flags
+    pub fn get_magnetic_flags(&mut self) -> Result<MagneticFlags, Error<SPI>> {
+        self.read_register(0x1b).map(From::from)
+    }
+
+    /// Get error flags
+    pub fn get_error_flags(&mut self) -> Result<ErrorFlags, Error<SPI>> {
+        self.read_register(0x1a).map(From::from)
     }
 
     /// Clear error flags
@@ -75,10 +189,12 @@ where
         self.cmd(0x20, 0x00).map(|_| ())
     }
 
-    fn cmd(&mut self, cmd: u8, arg: u8) -> Result<[u8; 4], Error<SPI>> {
-        let mut scratch = [cmd, arg, 0, 0];
-        self.tx(|spi| spi.transfer(&mut scratch))?;
-        Ok(scratch)
+    fn cmd(&mut self, reg: u8, arg: u8) -> Result<u8, Error<SPI>> {
+        let mut buf = [reg, arg];
+        self.tx(|spi| spi.transfer(&mut buf))?;
+        let mut buf = [0x00; 2];
+        self.tx(|spi| spi.transfer(&mut buf))?;
+        Ok(buf[1])
     }
 
     fn tx<RES, TX: FnOnce(&mut SPI) -> Result<RES, SPI::Error>>(
@@ -88,6 +204,151 @@ where
         self.cs.set_low().map_err(|_| Error::PinError)?;
         let res = tx(&mut self.spi).map_err(Error::TransferError);
         self.cs.set_high().map_err(|_| Error::PinError).and(res)
+    }
+}
+
+/// IRQ Mode
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum IRQMode {
+    LatchOff,
+    Logic,
+}
+
+/// Angle Change Interrupt Config
+#[derive(Clone, Copy, Debug)]
+pub struct AngleChangeInterrupt {
+    pub mode: IRQMode,
+    pub autoupdate: bool,
+    pub threshold: u8,
+    pub reference: u8,
+    pub hysteresis: u8,
+}
+
+impl AngleChangeInterrupt {
+    pub fn new(
+        mode: IRQMode,
+        autoupdate: bool,
+        threshold: u8,
+        reference: u8,
+        hysteresis: u8,
+    ) -> Self {
+        Self {
+            mode,
+            autoupdate,
+            threshold,
+            reference,
+            hysteresis,
+        }
+    }
+}
+
+/// Magnetic Field Thresholds Config
+#[derive(Clone, Copy, Debug)]
+pub struct MagneticFieldThresholds {
+    pub enable: bool,
+    pub low: u8,
+    pub high: u8,
+}
+
+impl MagneticFieldThresholds {
+    pub fn new(enable: bool, low: u8, high: u8) -> Self {
+        Self { enable, low, high }
+    }
+}
+
+/// Bias Current Trimming Config
+#[derive(Clone, Copy, Debug)]
+pub struct BiasCurrentTrimming {
+    pub val: u8,
+    pub trim_x: bool,
+    pub trim_y: bool,
+}
+
+impl BiasCurrentTrimming {
+    pub fn new(val: u8, trim_x: bool, trim_y: bool) -> Self {
+        Self {
+            val,
+            trim_x,
+            trim_y,
+        }
+    }
+}
+
+/// Error flags
+#[derive(Clone, Copy, Debug)]
+pub struct ErrorFlags {
+    pub nvm_error: bool,
+    pub memory_error: bool,
+    pub parity_error: bool,
+}
+
+impl ErrorFlags {
+    pub fn new(nvm_error: bool, memory_error: bool, parity_error: bool) -> Self {
+        Self {
+            nvm_error,
+            memory_error,
+            parity_error,
+        }
+    }
+}
+
+impl From<u8> for ErrorFlags {
+    fn from(val: u8) -> Self {
+        Self {
+            nvm_error: val & 0b10 > 0,
+            memory_error: val & 0b100 > 0,
+            parity_error: val & 0b1000 > 0,
+        }
+    }
+}
+
+/// Magnetic flags
+#[derive(Clone, Copy, Debug)]
+pub struct MagneticFlags {
+    pub low: bool,
+    pub high: bool,
+}
+
+impl MagneticFlags {
+    pub fn new(low: bool, high: bool) -> Self {
+        Self { low, high }
+    }
+}
+
+impl From<u8> for MagneticFlags {
+    fn from(val: u8) -> Self {
+        Self {
+            low: val & 0b1000000 > 0,
+            high: val & 0b10000000 > 0,
+        }
+    }
+}
+
+/// Rotation Direction
+#[derive(Clone, Copy, Debug)]
+pub enum Dir {
+    /// Clockwise
+    CW,
+    //Counterclockwise
+    CCW,
+}
+
+impl From<Dir> for u8 {
+    fn from(val: Dir) -> u8 {
+        match val {
+            Dir::CW => 0,
+            Dir::CCW => 0x80,
+        }
+    }
+}
+
+impl From<u8> for Dir {
+    fn from(val: u8) -> Self {
+        if val & 0x80 == 0x80 {
+            Self::CCW
+        } else {
+            Self::CW
+        }
     }
 }
 
